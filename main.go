91cb680764162e872b0c604b7b914783eaffb24f
Andre Leoni <andreluizleoni@gmail.com>
2021-09-07 15:53:44 -0300

    criado algoritmo de busca em profundadidade

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Grafo struct {
 	vertices int
@@ -27,6 +29,8 @@ func main() {
 	criaAresta(&grafo, 2, 4, 40)
 	criaAresta(&grafo, 3, 1, 3)
 	criaAresta(&grafo, 4, 3, 8)
+
+	profundidade(&grafo)
 
 	imprime(&grafo)
 }
@@ -74,4 +78,51 @@ func criaAresta(grafo *Grafo, vi int, vf int, p int) {
 	grafo.adj[vi].cab = novo
 
 	grafo.arestas++
+}
+
+const branco = 0
+const amarelo = 1
+const vermelho = 2
+
+// Busca por profundidade
+
+func profundidade(grafo *Grafo) {
+	size := grafo.vertices
+	result := []int{}
+
+	for i := 0; i <= size; i++ {
+		result = append(result, branco)
+	}
+
+	for i := 0; i <= size; i++ {
+		if result[i] == branco {
+			visitaP(grafo, i, &result)
+		}
+	}
+}
+
+func visitaP(grafo *Grafo, in int, result *[]int) {
+	new_result := *result
+	new_result[in] = amarelo
+	adj := grafo.adj[in].cab
+
+	for {
+		if new_result[in] == branco {
+			visitaP(grafo, adj.vertice, result)
+		}
+
+		if adj == nil {
+			break
+		}
+
+		adj = adj.prox
+
+		if adj == nil {
+			break
+		}
+	}
+
+	new_result[in] = vermelho
+
+	result = &new_result
 }
